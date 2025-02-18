cb7208e8ab9a776e198314c02d8b1fc004c46579
Oliver Bähler <oliverbaehler@hotmail.com>
2025-02-18 11:43:40 +0100

    feat(controller): add lifecycle for translator patches
    Signed-off-by: Oliver Bähler <oliverbaehler@hotmail.com>

--- e2e/suite_test.go
+++ e2e/suite_test.go
@@ -2,9 +2,12 @@
 package e2e_test
 
 import (
+	"context"
+
 	argocdv1alpha1 "github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/peak-scale/capsule-argo-addon/api/v1alpha1"
 	configv1alpha1 "github.com/peak-scale/capsule-argo-addon/api/v1alpha1"
 	capsulev1beta2 "github.com/projectcapsule/capsule/api/v1beta2"
 
@@ -18,9 +21,10 @@ import (
 )
 
 var (
-	cfg       *rest.Config
-	k8sClient client.Client
-	testEnv   *envtest.Environment
+	cfg                          *rest.Config
+	k8sClient                    client.Client
+	testEnv                      *envtest.Environment
+	originalArgoAddon, argoAddon *v1alpha1.ArgoAddon
 )
 
 var _ = BeforeSuite(func() {
@@ -50,10 +54,26 @@ var _ = BeforeSuite(func() {
 	Expect(CleanTenants(selector)).ToNot(HaveOccurred())
 	Expect(CleanTranslators(selector)).ToNot(HaveOccurred())
 
+	// Initialize originalArgoAddon before using it.
+	originalArgoAddon = &v1alpha1.ArgoAddon{}
+	err = k8sClient.Get(context.Background(), client.ObjectKey{Name: "default"}, originalArgoAddon)
+	Expect(err).ToNot(HaveOccurred())
+
+	argoAddon = originalArgoAddon.DeepCopy()
+
+	// Now that argoAddon is non-nil, you can safely update its spec.
+	argoAddon.Spec.Decouple = false
+	Expect(k8sClient.Update(context.Background(), argoAddon)).To(Succeed())
+
 	Eventually(CleanAppProjects(selector, "argocd"))
 })
 
 var _ = AfterSuite(func() {
+	// Apply the initial configuration from originalArgoAddon to argoaddon
+	Expect(k8sClient.Get(context.Background(), client.ObjectKey{Name: originalArgoAddon.Name}, argoAddon)).To(Succeed())
+	argoAddon.Spec = originalArgoAddon.Spec
+	Expect(k8sClient.Update(context.Background(), argoAddon)).To(Succeed())
+
 	By("tearing down the test environment")
 	Expect(testEnv.Stop()).ToNot(HaveOccurred())
 })
